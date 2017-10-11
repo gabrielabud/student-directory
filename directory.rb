17c1abb208ea569382f0bed3194dae7f765889fb
gabrielabud <gabriela.budeanu@gmail.com>
2017-10-11 15:34:38 +0100

    Chap5 methods update

--- directory.rb
+++ directory.rb
@@ -21,7 +21,12 @@ def print(names)
 names.each do |name|
   puts name
 end
+end
 
 def print_footer(names)
 puts "Overall, we have #{names.count} great students"
 end
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(studentss)
